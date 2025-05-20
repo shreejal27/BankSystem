b7b513afac09be0aa22d50abd1372646c9966c84
Shreejal Maharjan <shreejal27@gmail.com>
2025-05-20 23:14:02 +0545

    added necessary entities and enums

---
+++ BankSystem.Domain/Entities/User.cs
@@ -0,0 +1,15 @@
+﻿using BankSystem.Domain.Enums;
+
+namespace BankSystem.Domain.Entities
+{
+    public class User
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public Role Role { get; set; } = Role.User;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    }
+}
