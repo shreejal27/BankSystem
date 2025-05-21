03ce2a8b1e06dd18f80f24a7382363eb7661b1cc
Shreejal Maharjan <shreejal27@gmail.com>
2025-05-21 23:15:38 +0545

    added dbContext

---
+++ BankSystem.Infrastructure/Data/BankSystemDbContext.cs
@@ -0,0 +1,30 @@
+﻿using BankSystem.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BankSystem.Infrastructure.Data;
+
+public class BankSystemDbContext : DbContext
+{
+	public BankSystemDbContext(DbContextOptions<BankSystemDbContext> options)
+		: base(options) { }
+
+	public DbSet<User> Users => Set<User>();
+	public DbSet<Account> Accounts => Set<Account>();
+	public DbSet<Transaction> Transactions => Set<Transaction>();
+
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+		base.OnModelCreating(modelBuilder);
+
+		// Relationships
+		modelBuilder.Entity<Account>()
+			.HasOne(a => a.User)
+			.WithMany()   // If User has `List<Account> Accounts`, use `.WithMany(u => u.Accounts)`
+			.HasForeignKey(a => a.UserId);
+
+		modelBuilder.Entity<Transaction>()
+			.HasOne(t => t.Account)
+			.WithMany()
+			.HasForeignKey(t => t.AccountId);
+	}
+}
