543cf03bfed85065d6aab9a4aa00e7f1f0d030ef
Shreejal Maharjan <shreejal27@gmail.com>
2025-05-25 23:43:12 +0545

    created first migration file

---
+++ BankSystem.Infrastructure/Data/BankSystemDbContextFactory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace BankSystem.Infrastructure.Data
+{
+    public class BankSystemDbContextFactory : IDesignTimeDbContextFactory<BankSystemDbContext>
+    {
+        public BankSystemDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<BankSystemDbContext>();
+
+            // Replace with your actual connection string
+            var connectionString = "Server=HELLOWORLD\\SQLEXPRESS;Database=BankSystemDb;Trusted_Connection=True;TrustServerCertificate=True;";
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new BankSystemDbContext(optionsBuilder.Options);
+        }
+    }
+}
