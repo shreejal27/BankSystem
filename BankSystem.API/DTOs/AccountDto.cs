6137acf058722d1a51ccbe844d5a046189bdb6c4
Shreejal Maharjan <shreejal27@gmail.com>
2025-05-26 23:58:31 +0545

    created AccountDto, Interface and Services

---
+++ BankSystem.API/DTOs/AccountDto.cs
@@ -0,0 +1,8 @@
+﻿namespace BankSystem.API.DTOs
+{
+    public class CreateAccountDto
+    {
+        public string AccountNumber { get; set; }
+        public string HolderName { get; set; }
+    }
+}
