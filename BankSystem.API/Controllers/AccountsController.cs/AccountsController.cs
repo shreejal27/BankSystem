73d66dab5cff3ef41911dafd5bec4bd5728b5786
Shreejal Maharjan <shreejal27@gmail.com>
2025-05-28 23:25:21 +0545

    changed the names of action methods

--- BankSystem.API/Controllers/AccountsController.cs/AccountsController.cs
+++ BankSystem.API/Controllers/AccountsController.cs/AccountsController.cs
@@ -16,14 +16,14 @@ namespace BankSystem.API.Controllers.AccountsController.cs
         }
 
         [HttpPost]
-        public async Task<IActionResult> Create(CreateAccountDto dto)
+        public async Task<IActionResult> CreateAccount(CreateAccountDto dto)
         {
             var account = await _accountService.CreateAccountAsync(dto);
             return Ok(account);
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetAll()
+        public async Task<IActionResult> GetAllAccounts()
         {
             var accounts = await _accountService.GetAllAccountsAsync();
             return Ok(accounts);
