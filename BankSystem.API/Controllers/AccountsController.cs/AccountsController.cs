8e38f4219976fb03f29e2dd6f739dbb8f846d378
Shreejal Maharjan <shreejal27@gmail.com>
2025-06-02 22:46:11 +0545

    created update delete and get by id requests

--- BankSystem.API/Controllers/AccountsController.cs/AccountsController.cs
+++ BankSystem.API/Controllers/AccountsController.cs/AccountsController.cs
@@ -28,6 +28,28 @@ namespace BankSystem.API.Controllers.AccountsController.cs
             var accounts = await _accountService.GetAllAccountsAsync();
             return Ok(accounts);
         }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            var account = await _accountService.GetAccountByIdAsync(id);
+            if (account == null) return NotFound();
+            return Ok(account);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(Guid id, [FromBody] UpdateAccountDto dto)
+        {
+            var success = await _accountService.UpdateAccountAsync(id, dto);
+            return success ? NoContent() : NotFound();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var success = await _accountService.DeleteAccountAsync(id);
+            return success ? NoContent() : NotFound();
+        }
     }
 }
 
