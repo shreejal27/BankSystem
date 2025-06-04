1972814bb40079248c4d77e5dfe685128a304790
Shreejal Maharjan <shreejal27@gmail.com>
2025-06-04 20:24:00 +0545

    made dtos and services for register user

---
+++ BankSystem.Infrastructure/Services/UserService.cs
@@ -0,0 +1,33 @@
+﻿using BankSystem.Application.DTOs;
+using BankSystem.Application.Interfaces;
+using BankSystem.Domain.Entities;
+using BankSystem.Infrastructure.Data;
+
+namespace BankSystem.Infrastructure.Services
+{
+    public class UserService: IUserService
+    {
+        private readonly BankSystemDbContext _context;
+        public UserService(BankSystemDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> RegisterAsync(RegisterUserDto dto)
+        {
+            if (_context.Users.Any(u => u.Email == dto.Email))
+                return false;
+
+            var user = new User
+            {
+                Name = dto.Name,
+                Email = dto.Email,
+                Password = BCrypt.Net.BCrypt.HashPassword(dto.Password)
+            };
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+    }
+}
