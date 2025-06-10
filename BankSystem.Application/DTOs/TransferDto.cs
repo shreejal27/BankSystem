9636c9ae135274e4154cfc9e952a95b4efc3bbb2
Shreejal Maharjan <shreejal27@gmail.com>
2025-06-10 23:55:58 +0545

    made dtos for transactions module

---
+++ BankSystem.Application/DTOs/TransferDto.cs
@@ -0,0 +1,9 @@
+﻿namespace BankSystem.Application.DTOs
+{
+    public class TransferDto
+    {
+        public Guid FromAccountId { get; set; }
+        public Guid ToAccountId { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
