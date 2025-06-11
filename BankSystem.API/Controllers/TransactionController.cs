ad12554b716ce8d495ecac22889047004da0330a
Shreejal Maharjan <shreejal27@gmail.com>
2025-06-11 23:25:16 +0545

    made a transaction controller file

---
+++ BankSystem.API/Controllers/TransactionController.cs
@@ -0,0 +1,27 @@
+﻿using BankSystem.Application.DTOs;
+using BankSystem.Application.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BankSystem.API.Controllers
+{
+    public class TransactionController : ControllerBase
+    {
+        [ApiController]
+        [Route("api/[controller]")]
+        [Authorize]
+        private readonly ITransactionService _transactionService;
+
+        public TransactionController(ITransactionService transactionService)
+        {
+            _transactionService = transactionService;
+        }
+
+        [HttpPost("deposit")]
+        public async Task<IActionResult> Deposit([FromBody] DepositDto dto)
+        {
+            await _transactionService.DepositAsync(dto);
+            return Ok("Deposit successful.");
+        }
+    }
+}
