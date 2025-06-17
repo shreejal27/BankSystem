3836404cf0e895b4b7c0129ae3ae8efd5cf7c1d9
Shreejal Maharjan <shreejal27@gmail.com>
2025-06-17 21:48:12 +0545

    added transfer dto validator

---
+++ BankSystem.Application/Validators/TransferDtoValidator.cs
@@ -0,0 +1,15 @@
+﻿using BankSystem.Application.DTOs;
+using FluentValidation;
+
+namespace BankSystem.Application.Validators
+{
+    public class TransferDtoValidator : AbstractValidator<TransferDto>
+    {
+        public TransferDtoValidator()
+        {
+            RuleFor(x => x.FromAccountId).NotEmpty();
+            RuleFor(x => x.ToAccountId).NotEmpty().NotEqual(x => x.FromAccountId);
+            RuleFor(x => x.Amount).GreaterThan(0);
+        }
+    }
+}
