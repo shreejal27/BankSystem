2ebb3e215cfaaad82021f01b6be2bcb6e7ffd1b9
Shreejal Maharjan <shreejal27@gmail.com>
2025-06-17 21:46:20 +0545

    added withdraw dto validator

---
+++ BankSystem.Application/Validators/WithdrawDtoValidator.cs
@@ -0,0 +1,14 @@
+﻿using BankSystem.Application.DTOs;
+using FluentValidation;
+
+namespace BankSystem.Application.Validators
+{
+    public class WithdrawDtoValidator : AbstractValidator<WithdrawDto>
+    {
+        public WithdrawDtoValidator()
+        {
+            RuleFor(x => x.AccountId).NotEmpty();
+            RuleFor(x => x.Amount).GreaterThan(0);
+        }
+    }
+}
