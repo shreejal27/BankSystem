ec2f429ded4ee9f8f4f3f850a26316128ece70cb
Shreejal Maharjan <shreejal27@gmail.com>
2025-06-17 21:45:31 +0545

    added deposit dto validator file

---
+++ BankSystem.Application/Validators/DepositDtoValidator.cs
@@ -0,0 +1,14 @@
+﻿using BankSystem.Application.DTOs;
+using FluentValidation;
+
+namespace BankSystem.Application.Validators
+{
+    public class DepositDtoValidator : AbstractValidator<DepositDto>
+    {
+        public DepositDtoValidator()
+        {
+            RuleFor(x => x.AccountId).NotEmpty();
+            RuleFor(x => x.Amount).GreaterThan(0);
+        }
+    }
+}
