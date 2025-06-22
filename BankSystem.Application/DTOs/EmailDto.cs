f97b1e4fc8d5ec613c746178b42aefb752050534
Shreejal Maharjan <shreejal27@gmail.com>
2025-06-22 22:51:54 +0545

    made EmailDto and Email interface

---
+++ BankSystem.Application/DTOs/EmailDto.cs
@@ -0,0 +1,11 @@
+﻿namespace BankSystem.Application.DTOs
+{
+    public class EmailDto
+    {
+        public string SmtpServer { get; set; } = string.Empty;
+        public int Port { get; set; }
+        public string SenderEmail { get; set; } = string.Empty;
+        public string SenderName { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
