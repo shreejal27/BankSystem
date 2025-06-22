06670bd5c81a6ac365041f3cc6591b66bfb5da3b
Shreejal Maharjan <shreejal27@gmail.com>
2025-06-22 22:56:05 +0545

    implemented email service

---
+++ BankSystem.Infrastructure/Services/EmailService.cs
@@ -0,0 +1,31 @@
+﻿using BankSystem.Application.Interfaces;
+using Microsoft.Extensions.Options;
+using MimeKit;
+
+namespace BankSystem.Infrastructure.Services
+{
+    internal class EmailService : IEmailService
+    {
+        private readonly EmailSettings _settings;
+
+        public EmailService(IOptions<EmailSettings> settings)
+        {
+            _settings = settings.Value;
+        }
+
+        public async Task SendEmailAsync(string toEmail, string subject, string body)
+        {
+            var email = new MimeMessage();
+            email.From.Add(new MailboxAddress(_settings.SenderName, _settings.SenderEmail));
+            email.To.Add(MailboxAddress.Parse(toEmail));
+            email.Subject = subject;
+            email.Body = new TextPart("plain") { Text = body };
+
+            using var smtp = new SmtpClient();
+            await smtp.ConnectAsync(_settings.SmtpServer, _settings.Port, MailKit.Security.SecureSocketOptions.StartTls);
+            await smtp.AuthenticateAsync(_settings.SenderEmail, _settings.Password);
+            await smtp.SendAsync(email);
+            await smtp.DisconnectAsync(true);
+        }
+    }
+}
