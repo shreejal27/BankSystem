8010af3f0f5f51110030b581d8f9b7ecb6ecbfe9
Shreejal Maharjan <shreejal27@gmail.com>
2025-06-29 21:36:12 +0545

    made registerUserDto Validator

---
+++ BankSystem.Application/Validators/RegisterUserDtoValidator.cs
@@ -0,0 +1,24 @@
+﻿using BankSystem.Application.DTOs;
+using FluentValidation;
+
+namespace BankSystem.Application.Validators
+{
+    public class RegisterUserDtoValidator : AbstractValidator<RegisterUserDto>
+    {
+        public RegisterUserDtoValidator()
+        {
+            RuleFor(x => x.Email)
+                .NotEmpty()
+                .WithMessage("Email is required.")
+                .EmailAddress()
+                .WithMessage("Invalid email format.");
+
+            RuleFor(x => x.Name)
+                .NotEmpty();
+
+            RuleFor(x => x.Password)
+                .NotEmpty()
+                .MinimumLength(8);
+        }
+    }
+}
