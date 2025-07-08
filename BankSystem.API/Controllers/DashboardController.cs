cbcaad724056ef9d99f8a4e685384160aaab0170
Shreejal Maharjan <shreejal27@gmail.com>
2025-07-08 23:39:30 +0545

    made the dashboardController

---
+++ BankSystem.API/Controllers/DashboardController.cs
@@ -0,0 +1,28 @@
+﻿using BankSystem.Application.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace BankSystem.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DashboardController : ControllerBase
+    {
+        private readonly IDashboardService _dashboardService;
+        public DashboardController(IDashboardService dashboardService)
+        {
+            _dashboardService = dashboardService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetDashboard()
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            if (string.IsNullOrEmpty(userId)) return Unauthorized();
+
+            var dashboard = await _dashboardService.GetUserDashboardAsync(Guid.Parse(userId));
+            return Ok(dashboard);
+        }
+    }
+}
