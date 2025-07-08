335d248b72b7836a978480ade1abdb6a48414217
Shreejal Maharjan <shreejal27@gmail.com>
2025-07-08 23:22:57 +0545

    made Dashboard Dto

---
+++ BankSystem.Application/DTOs/DashboardDto.cs
@@ -0,0 +1,10 @@
+﻿namespace BankSystem.Application.DTOs
+{
+    public class DashboardDto
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public decimal TotalBalance { get; set; }
+        public IEnumerable<TransactionDto> RecentTransactions { get; set; } = new List<TransactionDto>();
+    }
+}
