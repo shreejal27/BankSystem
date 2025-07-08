60da643c2f5099b50aac7e0b8f47a4c5dce07172
Shreejal Maharjan <shreejal27@gmail.com>
2025-07-08 23:25:01 +0545

    made interface class for DashboardService

---
+++ BankSystem.Application/Interfaces/IDashboardService.cs
@@ -0,0 +1,9 @@
+﻿using BankSystem.Application.DTOs;
+
+namespace BankSystem.Application.Interfaces
+{
+    public interface IDashboardService
+    {
+        Task<DashboardDto> GetUserDashboardAsync(Guid userId);
+    }
+}
