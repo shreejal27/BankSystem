8866a6c0113b9fdb07cafa89e76eee69d3313d5a
Shreejal Maharjan <shreejal27@gmail.com>
2025-07-10 23:57:45 +0545

    made deactivate api for adming

---
+++ BankSystem.API/Controllers/AdminController.cs
@@ -0,0 +1,22 @@
+﻿using BankSystem.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BankSystem.API.Controllers
+{
+    public class AdminController : ControllerBase
+    {
+        private readonly IAdminService _adminService;
+
+        public AdminController(IAdminService adminService)
+        {
+            _adminService = adminService;
+        }
+
+        [HttpPut("users/{id}/deactivate")]
+        public async Task<IActionResult> DeactivateUser(Guid id)
+        {
+            await _adminService.DeactivateUserAsync(id);
+            return Ok(new { message = "User deactivated successfully" });
+        }
+    }
+}
