5377af071f51168f73f43a45cf55a4de99edf148
Shreejal Maharjan <shreejal27@gmail.com>
2025-07-10 23:50:23 +0545

    made deactivate user task

---
+++ BankSystem.Infrastructure/Services/AdminService.cs
@@ -0,0 +1,23 @@
+﻿using BankSystem.Application.Interfaces;
+using BankSystem.Infrastructure.Data;
+
+namespace BankSystem.Infrastructure.Services
+{
+    public class AdminService : IAdminService
+    {
+        private readonly BankSystemDbContext _context;
+
+        public AdminService(BankSystemDbContext context)
+        {
+            _context = context;
+        }
+        public async Task DeactivateUserAsync(Guid userId)
+        {
+            var user = await _context.Users.FindAsync(userId)
+                ?? throw new Exception("User not found");
+
+            user.IsActive = false;
+            await _context.SaveChangesAsync();
+        }
+    }
+}
