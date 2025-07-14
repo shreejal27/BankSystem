95935ead35d40dcd0f33bacbec37fb0faeb091d6
Shreejal Maharjan <shreejal27@gmail.com>
2025-07-14 22:14:37 +0545

    added the task in admin interface

--- BankSystem.Application/Interfaces/IAdminService.cs
+++ BankSystem.Application/Interfaces/IAdminService.cs
@@ -1,8 +1,11 @@
-﻿namespace BankSystem.Application.Interfaces
+﻿using System.Transactions;
+
+namespace BankSystem.Application.Interfaces
 {
     public interface IAdminService
     {
         Task<int> GetTotalUsersAsync();
         Task DeactivateUserAsync(Guid userId);
+        Task<IEnumerable<Transaction>> GetAllTransactionsAsync();
     }
 }
