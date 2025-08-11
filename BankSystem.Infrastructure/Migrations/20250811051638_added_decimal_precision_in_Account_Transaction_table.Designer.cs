b8d9aae188935c1eff7d1413dc46416dd7c61080
Shreejal Maharjan <shreejal27@gmail.com>
2025-08-11 11:02:40 +0545

    created migration file for decimal precision in Account and Transaction table

--- BankSystem.Infrastructure/Migrations/20250811051638_added_decimal_precision_in_Account_Transaction_table.Designer.cs
+++ BankSystem.Infrastructure/Migrations/20250811051638_added_decimal_precision_in_Account_Transaction_table.Designer.cs
@@ -4,6 +4,7 @@ using BankSystem.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BankSystem.Infrastructure.Migrations
 {
     [DbContext(typeof(BankSystemDbContext))]
-    partial class BankSystemDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250811051638_added_decimal_precision_in_Account_Transaction_table")]
+    partial class added_decimal_precision_in_Account_Transaction_table
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ BankSystem.Infrastructure/Migrations/20250811051638_added_decimal_precision_in_Account_Transaction_table.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BankSystem.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class added_decimal_precision_in_Account_Transaction_table : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Users",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Users");
+        }
+    }
+}
