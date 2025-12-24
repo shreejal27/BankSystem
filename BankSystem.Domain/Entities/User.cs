682bb9e2560abf0af7b856201102ca04c325f300
Shreejal Maharjan <shreejal27@gmail.com>
2025-12-24 23:45:01 +0545

    added NeedEmail condition to User Table

--- BankSystem.Domain/Entities/User.cs
+++ BankSystem.Domain/Entities/User.cs
@@ -10,7 +10,7 @@ namespace BankSystem.Domain.Entities
         public string Password { get; set; } = string.Empty;
         public Role Role { get; set; } = Role.User;
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-        public bool? IsActive { get; set; } 
-
+        public bool? IsActive { get; set; }
+        public bool NeedEmail { get; set; } = false;
     }
 }
