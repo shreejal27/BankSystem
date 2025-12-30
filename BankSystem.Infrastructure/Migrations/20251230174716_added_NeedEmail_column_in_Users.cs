a6c640454adba34c0b1cc496a3df6444724d47df
Shreejal Maharjan <shreejal27@gmail.com>
2025-12-30 23:32:56 +0545

    created migration file and updated in database

---
+++ BankSystem.Infrastructure/Migrations/20251230174716_added_NeedEmail_column_in_Users.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BankSystem.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class added_NeedEmail_column_in_Users : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "NeedEmail",
+                table: "Users",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NeedEmail",
+                table: "Users");
+        }
+    }
+}
